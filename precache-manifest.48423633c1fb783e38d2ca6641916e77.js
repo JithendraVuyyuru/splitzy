da7729a63e9360d886ac03386d8d0bb104836413
JithendraVuyyuru <jithendraadesign@gmail.com>
2025-02-17 04:00:14 +0530

    Updates

---
+++ precache-manifest.48423633c1fb783e38d2ca6641916e77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cedf5d67e857eaa4211d18e2bf9bae",
+    "url": "/splitzy/index.html"
+  },
+  {
+    "revision": "328ecb98c31d9de9fc1b",
+    "url": "/splitzy/static/css/main.980694f1.chunk.css"
+  },
+  {
+    "revision": "9ab18ddfe253f15d07d9",
+    "url": "/splitzy/static/js/2.5db0f54c.chunk.js"
+  },
+  {
+    "revision": "d4b4259a290c788aec291821574a2b0b",
+    "url": "/splitzy/static/js/2.5db0f54c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f92a28da1ad9aedf16",
+    "url": "/splitzy/static/js/3.3b68c2b9.chunk.js"
+  },
+  {
+    "revision": "328ecb98c31d9de9fc1b",
+    "url": "/splitzy/static/js/main.229d82e5.chunk.js"
+  },
+  {
+    "revision": "49e8d453b2ea802da703",
+    "url": "/splitzy/static/js/runtime-main.a6b0c79e.js"
+  }
+]);
